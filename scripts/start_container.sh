7a5b8b03699971fd0f8fb185dd9d0de87dcc0659
Rakeshilpa <91446780+Rakeshilpa@users.noreply.github.com>
2024-07-07 16:35:55 +0530

    added the file code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull rakeshilpa123/digital_marketing_tween_agency:latest
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 rakeshilpa123/digital_marketing_tween_agency:latest
