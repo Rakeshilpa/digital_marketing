a0c9f968300fd5863a7e7b80798490061b206322
Rakeshilpa <91446780+Rakeshilpa@users.noreply.github.com>
2024-07-07 18:59:52 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -5,18 +5,4 @@ set -e
 IMAGE_NAME="rakeshilpa123/digital_marketing_tween_agency:latest"
 CONTAINER_NAME="heuristic_raman"
 
-# Stop the container if it is running
-if [ $(docker ps -q -f name=$CONTAINER_NAME) ]; then
-    echo "Stopping container: $CONTAINER_NAME"
-    docker stop $CONTAINER_NAME
-fi
-
-# Remove the container if it exists
-if [ $(docker ps -a -q -f name=$CONTAINER_NAME) ]; then
-    echo "Removing container: $CONTAINER_NAME"
-    docker rm $CONTAINER_NAME
-fi
-
-# Remove any dangling images (optional, for cleanup)
-echo "Removing dangling images"
-docker image prune -f
+docker rm -f $CONTAINER_NAME
