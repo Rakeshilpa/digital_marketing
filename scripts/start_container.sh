be0a30f54e5bf83fd2bf5e60937bdfa244b40ee5
Rakeshilpa <91446780+Rakeshilpa@users.noreply.github.com>
2024-07-07 17:01:52 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# Pull the Docker image from Docker Hub 
 docker pull rakeshilpa123/digital_marketing_tween_agency:latest
 
 # Run the Docker image as a container
